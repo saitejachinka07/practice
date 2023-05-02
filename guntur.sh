f01fa1d7f0535040e386023588510c9cbe0225a2
saitej <chinka.saiteja@gmail.com>
2023-05-02 22:35:43 +0530

    tried my own

---
+++ guntur.sh
@@ -0,0 +1,22 @@
+
+
+
+input=$1
+
+if [ -z "$input"]; then
+echo Input is missing
+exit
+fi
+
+ hello=$(curl -s https://github.com/raghudevopsb72/$input | grep "01-sample.sh" | xargs -n1 | grep title | awk -F = '{print $2}')
+
+echo hello -$hello
+
+if [ "$hello" == learn-shell]
+then
+    echo "script is executed"
+    else
+    echo "need to improve my understanding"
+
+    fi
+
